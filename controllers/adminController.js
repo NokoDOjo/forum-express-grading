458da0a3c915c503ad1927b75d322fd1bb4b745a
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-08-25 22:12:44 +0800

    feat: add admin restaurant post

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,6 +6,21 @@ const adminController = {
     return Restaurant.findAll({raw: true}).then(restaurants => {
       return res.render('admin/restaurants', {restaurants: restaurants })
     })
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
+  },
+  postRestaurant: (req, res) => {
+    const { name, tel, address, opening_hours, description } = req.body
+    if ( !name || !tel || !address || !opening_hours || !description ) {
+      req.flash('error_messages', "Please fill in all the blanks")
+      return res.redirect('back')
+    }
+    return Restaurant.create({ name, tel, address, opening_hours, description })
+      .then((restaurant) => {
+        req.flash('success_messages', 'restaurants was  successfully created')
+        res.redirect('/admin/restaurants')
+      })
   }
 }
 

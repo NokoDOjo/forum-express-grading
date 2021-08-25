bffe405f166bd36f853b44e833812ea94d3688e5
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-08-25 22:15:54 +0800

    add admin restaurant page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -21,6 +21,12 @@ const adminController = {
         req.flash('success_messages', 'restaurants was  successfully created')
         res.redirect('/admin/restaurants')
       })
+  },
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { raw:true })
+      .then(restaurant => {
+        return res.render('admin/restaurant', { restaurant })
+      })
   }
 }
 
